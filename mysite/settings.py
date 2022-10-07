9bec694e3fe72903cd42081b1a5ae08155e8c41e
anl8rpm <anl8rpm@virginia.edu>
2022-10-06 21:07:06 -0400

    Minor changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'welcome',
+    'welcome.apps.WelcomeConfig',
     'bootstrap5',
 ]
 
