dfbe1f43c63f97fbe2a920aa0be2fee4446039a2
anl8rpm <anl8rpm@virginia.edu>
2022-10-06 21:06:42 -0400

    Implemented Test Case

--- welcome/tests.py
+++ welcome/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from allauth.account.models import EmailAddress
+from django.contrib.auth.models import User
 
-# Create your tests here.
+class GoogleLoginTests(TestCase):
+    def test_is_not_logged_in(self):
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Login with Google")
+
