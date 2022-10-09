389fa66788fd66324053c3eb52e88b933b595fa9
Weilian Wu <ww9uq@virginia.edu>
2022-10-09 14:42:48 -0400

    Fix error

--- welcome/urls.py
+++ welcome/urls.py
@@ -2,10 +2,9 @@ from django.urls import path, include
 from django.contrib.auth.views import LogoutView
 from . import views
 
-app_name = 'Study Buddy Finder App'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view(), name='logout'),
+    path('logout', LogoutView.as_view()),
 ]
