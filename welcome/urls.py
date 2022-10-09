03ac018be1b431f35dbd9bcbe2fd1592e9ed4add
Weilian Wu <ww9uq@virginia.edu>
2022-10-09 14:36:10 -0400

    small changes

--- welcome/urls.py
+++ welcome/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from django.contrib.auth.views import LogoutView
-
 from . import views
+
+app_name = 'Study Buddy Finder App'
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('accounts/', include('allauth.urls')),
-    path('logout', LogoutView.as_view()),
+    path('logout', LogoutView.as_view(), name='logout'),
 ]
