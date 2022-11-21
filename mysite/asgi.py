2e9537dbdc42ef509d49ca68fdbda38cf8f9f7ab
anl8rpm <anl8rpm@virginia.edu>
2022-11-21 14:33:54 -0500

    fixed heroku deploy error

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,4 +1,5 @@
 import os
+import django
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
@@ -6,9 +7,9 @@ from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+django.setup()
 
 import welcome.routing
-
 
 django_asgi_app = get_asgi_application()
 
