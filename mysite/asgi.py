6d2d2268f7242dcdfa5c58a9e5decc4749d4b3c8
anl8rpm <anl8rpm@virginia.edu>
2022-11-21 13:54:32 -0500

    minor bug fixes

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -1,13 +1,13 @@
 import os
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import welcome.routing
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 django_asgi_app = get_asgi_application()
 
