08ee9e507bc0d1982f6c1fd2e5611f4e6762dfde
anl8rpm <anl8rpm@virginia.edu>
2022-12-06 21:10:28 -0500

    Update tests.py

--- welcome/tests.py
+++ welcome/tests.py
@@ -9,6 +9,8 @@ def create_user(username):
     user.set_password('test')
     user.save()
 
+    return user
+
 class GoogleLoginTests(TestCase):
     def test_is_not_logged_in(self):
         response = self.client.get(reverse('index'))
@@ -58,3 +60,37 @@ class ProfileViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "CS 3100 LEC Section 2 w/ Mark Floryan")
         self.assertContains(response, "CS 3240 LEC Section 1 w/ Paul McBurney")
+
+class IndexViewTests(TestCase):
+    def test_no_classes(self):
+        user = create_user("test")
+        self.client.login(username='test', password='test')
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "You have no classes added to your profile.")
+        self.assertContains(response, "You are not currently available for any classes.")
+        self.assertContains(response, "You currently have no upcoming meetings")
+
+    def test_one_class_unavailble(self):
+        user = create_user("test")
+
+        self.client.login(username='test', password='test')
+        response = self.client.get(reverse('index'))
+        create_class(response.wsgi_request.user, "CS", 3100, "LEC", 2, "Mark Floryan")
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "CS 3100 LEC Section 2 w/ Mark Floryan")
+        self.assertContains(response, "You are not currently available for any classes.")
+
+    def test_one_class_available(self):
+        user = create_user("test")
+
+        self.client.login(username='test', password='test')
+        response = self.client.get(reverse('index'))
+        c = create_class(response.wsgi_request.user, "CS", 3100, "LEC", 2, "Mark Floryan")
+        self.client.post('/update', data={"class_to_update": [c.id]})
+        response = self.client.get(reverse('index'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "CS 3100 LEC Section 2 w/ Mark Floryan")
+        self.assertNotContains(response, "You are not currently available for any classes.")
+
